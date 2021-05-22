d3448c669b0dd93bec5af264b635fd119ea98f78
Nathan Wichers <bluroseai@hotmail.com>
2021-05-22 16:11:17 -0400

    new text changes
    just need to do move descriptions, send help

--- scripts/make.bat
+++ scripts/make.bat
@@ -3,3 +3,9 @@ tools\armips data\moves.s
 tools\narchive create a011.narc a011/ -nf
 rmdir /q /s a011
 armips asm\moves.s
+cd text
+thenewpoketext < script1.txt
+xcopy "a027withalotofblanks" "tmp_base\root\a\0\2\7"
+thenewpoketext < script2.txt
+cd ..\
+xcopy "text\tmp_base\a027.narc" "a027.narc"
