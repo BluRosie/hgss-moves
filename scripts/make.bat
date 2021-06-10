943a5e9e36f0cada206caba0c70a932955b456c8
Nathan <bluroseai@hotmail.com>
2021-06-10 07:56:01 -0400

    update make.bat to make the asm files properly

--- scripts/make.bat
+++ scripts/make.bat
@@ -2,7 +2,8 @@ mkdir a011
 tools\armips data\moves.s
 tools\narchive create a011.narc a011/ -nf
 rmdir /q /s a011
-armips asm\moves.s
+tools\armips asm\moves.s
+tools\armips asm\tr_ai.s
 cd text
 thenewpoketext < script1.txt
 xcopy "a027withalotofblanks" "tmp_base\root\a\0\2\7"
