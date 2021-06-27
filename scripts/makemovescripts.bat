6b0c41f2a31230af4789f2bec65ddd3444b68bc3
Nathan Wichers <bluroseai@hotmail.com>
2021-06-27 14:18:34 -0400

    better rename script folders

--- scripts/makemovescripts.bat
+++ scripts/makemovescripts.bat
@@ -1,22 +1,22 @@
 mkdir a030 a001
 for /l %%F in (0, 1, 9) do (
- if exist "movescripts\a030\00%%F.s" (
-  tools\armips movescripts\a030\00%%F.s ))
+ if exist "movescripts\battlemovescripts\00%%F.s" (
+  tools\armips movescripts\battlemovescripts\00%%F.s ))
 for /l %%F in (10, 1, 99) do (
- if exist "movescripts\a030\0%%F.s" (
-  tools\armips movescripts\a030\0%%F.s ))
+ if exist "movescripts\battlemovescripts\0%%F.s" (
+  tools\armips movescripts\battlemovescripts\0%%F.s ))
 for /l %%F in (100, 1, 700) do (
- if exist "movescripts\a030\%%F.s" (
-  tools\armips movescripts\a030\%%F.s ))
+ if exist "movescripts\battlemovescripts\%%F.s" (
+  tools\armips movescripts\battlemovescripts\%%F.s ))
 for /l %%F in (0, 1, 9) do (
- if exist "movescripts\a001\00%%F.s" (
-  tools\armips movescripts\a001\00%%F.s ))
+ if exist "movescripts\battlesubscripts\00%%F.s" (
+  tools\armips movescripts\battlesubscripts\00%%F.s ))
 for /l %%F in (10, 1, 99) do (
- if exist "movescripts\a001\0%%F.s" (
-  tools\armips movescripts\a001\0%%F.s ))
+ if exist "movescripts\battlesubscripts\0%%F.s" (
+  tools\armips movescripts\battlesubscripts\0%%F.s ))
 for /l %%F in (100, 1, 700) do (
- if exist "movescripts\a001\%%F.s" (
-  tools\armips movescripts\a001\%%F.s ))
+ if exist "movescripts\battlesubscripts\%%F.s" (
+  tools\armips movescripts\battlesubscripts\%%F.s ))
 tools\narchive create a030.narc a030 -nf
 tools\narchive create a001.narc a001 -nf
 rmdir /q /s a001
