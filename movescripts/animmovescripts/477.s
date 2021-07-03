75e26280b65e2689a3d5ec707d99a79ca40459ec
Nathan Wichers <bluroseai@hotmail.com>
2021-07-03 16:32:23 -0400

    autotomize animation
    touch up w venoshock as well

--- movescripts/animmovescripts/477.s
+++ movescripts/animmovescripts/477.s
@@ -9,6 +9,10 @@
 
 .create "a010\a010_477", 0
 
+red equ 20
+green equ 0
+blue equ 31
+
 a010_477:
     loadparticlefromspa 0, 491
     waitparticle
@@ -20,8 +24,12 @@ a010_477:
     playsepan 1859, 117
     wait 48
     callfunction 36, 5, 3, 0, 1, 10, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    callfunction 34, 5, 8, 1, 1, red | green << 5 | blue << 10, 12, "NaN", "NaN", "NaN", "NaN", "NaN"
     repeatse 1979, 117, 2, 10
-    wait 72
+    callfunction 34, 5, 8, 1, 1, red | green << 5 | blue << 10, 12, "NaN", "NaN", "NaN", "NaN", "NaN"
+	wait 12
+    callfunction 34, 5, 8, 1, 1, red | green << 5 | blue << 10, 12, "NaN", "NaN", "NaN", "NaN", "NaN"
+    wait 48
 
     unloadparticle 0
     waitstate
--- movescripts/animmovescripts/478.s
+++ movescripts/animmovescripts/478.s
@@ -9,22 +9,31 @@
 
 .create "a010\a010_478", 0
 
+red equ 31
+green equ 31
+blue equ 31
+
 a010_478:
-    loadparticlefromspa 0, 486
+    loadparticlefromspa 0, 492
     waitparticle
 
-    addparticle 0, 2, 3
-    addparticle 0, 1, 3
+	callfunction 34, 6, 2, 0, 1, red | green << 5 | blue << 10, 10, 10, "NaN", "NaN", "NaN", "NaN"
+    callfunction 36, 5, 3, 0, 1, 7, 264, "NaN", "NaN", "NaN", "NaN", "NaN" // shake target mon
     addparticle 0, 1, 3
     addparticle 0, 0, 3
-    wait 10
-
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
-    wait 10
-
-    addparticle 0, 2, 3
-    addparticle 0, 0, 3
+    playsepan 1911, -117
+	wait 2
+    playsepan 1911, -117
+	wait 2
+    playsepan 1911, -117
+	wait 2
+    playsepan 1911, -117
+	wait 2
+    playsepan 1911, -117
+	wait 2
+    playsepan 1911, -117
+	wait 2
+    playsepan 1911, -117
     waitparticle
 
     unloadparticle 0
