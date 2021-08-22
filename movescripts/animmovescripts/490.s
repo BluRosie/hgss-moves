2f30dba4689c9a1fd9dfdb0ef2f21746728d2582
Nathan Wichers <bluroseai@hotmail.com>
2021-08-21 22:15:05 -0400

    soak

--- movescripts/animmovescripts/490.s
+++ movescripts/animmovescripts/490.s
@@ -9,27 +9,27 @@
 
 .create "a010\a010_490", 0
 
+red equ 0
+green equ 0
+blue equ 31
+
 a010_490:
-    loadparticlefromspa 0, 486
+    loadparticlefromspa 0, 503 // new soak spa
     waitparticle
 
-    addparticle 0, 2, 3
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
-    addparticle 0, 0, 3
-    wait 10
+    addparticle 0, 2, 4
+    addparticle 0, 1, 4
+    addparticle 0, 0, 4
+    wait 30
 
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
-    wait 10
-
-    addparticle 0, 2, 3
-    addparticle 0, 0, 3
+    addparticle 0, 3, 4
+    callfunction 36, 5, 4, 0, 1, 12, 264, "NaN", "NaN", "NaN", "NaN", "NaN" // shake mon
+    callfunction 34, 5, 8, 1, 1, red | green << 5 | blue << 10, 12, "NaN", "NaN", "NaN", "NaN", "NaN" // shades target mon rgb555 color
     waitparticle
 
     unloadparticle 0
     waitstate
     end
-    
+
 
 .close
