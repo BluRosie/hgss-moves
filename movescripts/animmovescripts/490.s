7db22d87dcdfa0113311fe8bb1e9913e10e76c67
Nathan Wichers <bluroseai@hotmail.com>
2021-08-22 20:02:21 -0400

    flame charge

--- movescripts/animmovescripts/490.s
+++ movescripts/animmovescripts/490.s
@@ -17,11 +17,13 @@ a010_490:
     loadparticlefromspa 0, 503 // new soak spa
     waitparticle
 
+// shower
     addparticle 0, 2, 4
     addparticle 0, 1, 4
     addparticle 0, 0, 4
     wait 30
 
+// hit
     addparticle 0, 3, 4
     callfunction 36, 5, 4, 0, 1, 12, 264, "NaN", "NaN", "NaN", "NaN", "NaN" // shake mon
     callfunction 34, 5, 8, 1, 1, red | green << 5 | blue << 10, 12, "NaN", "NaN", "NaN", "NaN", "NaN" // shades target mon rgb555 color
