e6229326e582434202c0295db3e44e0afa80eae6
Nathan Wichers <bluroseai@hotmail.com>
2021-08-24 00:06:46 -0400

    coil (70 left)

--- movescripts/animmovescripts/492.s
+++ movescripts/animmovescripts/492.s
@@ -9,27 +9,24 @@
 
 .create "a010\a010_492", 0
 
+red equ 31
+green equ 31
+blue equ 31
+
 a010_492:
-    loadparticlefromspa 0, 486
+    loadparticlefromspa 0, 505
     waitparticle
 
-    addparticle 0, 2, 3
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
+// add ring that goes around the user
     addparticle 0, 0, 3
-    wait 10
-
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
-    wait 10
-
-    addparticle 0, 2, 3
-    addparticle 0, 0, 3
+    wait 20
+// turn mon white
+    callfunction 34, 6, 2, 0, 1, red | green << 5 | blue << 10, 10, 10, "NaN", "NaN", "NaN", "NaN"
     waitparticle
 
     unloadparticle 0
     waitstate
     end
-    
+
 
 .close
